9c813bf9829211c4d9f9191e0a11dabab2029ce2
proAlvaroSam <pro.alvaro.sam@gmail.com>
2024-12-18 12:36:21 -0300

    primeira atividade
    Minha versão do código da primeira aula, com base nos conhecimentos adquiridos no curso de lógica da programação.

---
+++ melhoria-do-codigo-da-aula01.js
@@ -0,0 +1,12 @@
+const idade = 20;
+
+//segui a mesma ideia do código original, mostrado no vídeo.
+//Com a diferença que nesse, eu usei o meu conhecimento
+//adquirido com a lógica de programação.
+const frase = 'sua idade';
+console.log(`\nSua idade e suas variações\n`)
+console.log(`${frase} original : ${idade}`)
+console.log(`${frase} mais ela mesma : ${idade + idade}`)
+console.log(`${frase} dividida por 2 : ${idade / 2}`)
+console.log(`${frase} multiplicada por 2 : ${idade * 2}`)
+console.log(`${frase} menos a metade dela : ${idade - (idade / 2)}`)
